1cd2fca703fc577c52b20d542f15d769f03e3760
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2014-12-24 12:58:43 +0200

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Radu Croitoru on 24/12/14.
+//  Copyright (c) 2014 Radu Croitoru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Radu Croitoru on 24/12/14.
+//  Copyright (c) 2014 Radu Croitoru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
